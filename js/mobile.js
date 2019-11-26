9ecbf93bee560cd38e19442d29d315b2016c49b8
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2019-11-26 13:47:02 -0500

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,12 @@
 
 (function($) {
     //Home Button
-    $("#button").vclick(function(){
+    $("#button").click(function(){
         $(this).css("background-color","white");
         $(this).css("color","rgb(29, 29, 29)");
         $(this).css("transition",".3s");
     });
+
 })(jQuery);
 
 
