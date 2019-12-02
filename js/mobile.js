9250567f62cc2582c75ec06e92d755aa73c5d403
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2019-12-02 01:42:32 -0500

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -4,6 +4,7 @@ jQuery(function ($) { "use strict";
   //Smooth Scrolling
   var scroll = new SmoothScroll('a[href*="#"]');
   
+  //Swiper 
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
@@ -23,6 +24,21 @@ jQuery(function ($) { "use strict";
     },
 
   });
+
+  //Menu
+  $("#menuicon").hover(function(){
+    $("#menu").css("visibility","visible");
+    $("#menu").css("transform","translateX(500px)")
+    $("#menu").css("-webkit-transform","translateX(500px)")
+  },function(){
+    $("#menu").css("transform","translateX(0px)");
+    $("#menu").css("-webkit-transform","translateX(0px)")
+    setTimeout(function(){
+      $("#menu").css("visibility", "hidden");
+    }, 500);
+  });
+
+
 
 });
 
